ed639914175116edd6a819d107db6fd1619fe5b5
Dilson Rabelo <dilsonjlrjr@gmail.com>
2021-06-09 11:15:31 -0300

    Button Leitor

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Grid, makeStyles } from '@material-ui/core';
 
-const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        POC Barcode
-      </a>
-    </header>
-  </div>
-);
+const useMyStyles = makeStyles({
+  grid: {
+    height: '100vh',
+  },
+});
+
+const App: React.FC = () => {
+  const classes = useMyStyles();
+
+  return (
+    <Grid
+      className={classes.grid}
+      container
+      direction="row"
+      justify="center"
+      alignContent="center"
+    >
+      <Button variant="contained" color="primary">
+        Iniciar leitor
+      </Button>
+    </Grid>
+  );
+};
 
 export default App;
