ea5670dc77dc0b52e4c85fb00346e972755e32c9
Dilson Rabelo <dilsonjlrjr@gmail.com>
2021-06-17 11:30:58 -0300

    Ajuste final

--- src/Scanner/index.tsx
+++ src/Scanner/index.tsx
@@ -51,6 +51,7 @@ const Scanner: React.FC<ScannerProps> = (props: ScannerProps) => {
     Quagga.init(
       {
         inputStream: {
+          target: '#barcodeContainer',
           type: 'LiveStream',
           constraints: {
             width: 640,
@@ -102,47 +103,47 @@ const Scanner: React.FC<ScannerProps> = (props: ScannerProps) => {
       },
     );
 
-    // Quagga.onProcessed(result => {
-    //   const drawingCtx = Quagga.canvas.ctx.overlay;
-    //   const drawingCanvas = Quagga.canvas.dom.overlay;
+    Quagga.onProcessed(result => {
+      const drawingCtx = Quagga.canvas.ctx.overlay;
+      const drawingCanvas = Quagga.canvas.dom.overlay;
 
-    //   if (result) {
-    //     if (result.boxes) {
-    //       drawingCtx.clearRect(
-    //         0,
-    //         0,
-    //         Number(drawingCanvas.getAttribute('width')),
-    //         Number(drawingCanvas.getAttribute('height')),
-    //       );
-    //       result.boxes
-    //         .filter(box => {
-    //           return box !== result.box;
-    //         })
-    //         .forEach(box => {
-    //           Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
-    //             color: 'green',
-    //             lineWidth: 2,
-    //           });
-    //         });
-    //     }
+      if (result) {
+        if (result.boxes) {
+          drawingCtx.clearRect(
+            0,
+            0,
+            Number(drawingCanvas.getAttribute('width')),
+            Number(drawingCanvas.getAttribute('height')),
+          );
+          result.boxes
+            .filter(box => {
+              return box !== result.box;
+            })
+            .forEach(box => {
+              Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+                color: 'green',
+                lineWidth: 2,
+              });
+            });
+        }
 
-    //     if (result.box) {
-    //       Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
-    //         color: '#00F',
-    //         lineWidth: 2,
-    //       });
-    //     }
+        if (result.box) {
+          Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
+            color: '#00F',
+            lineWidth: 2,
+          });
+        }
 
-    //     if (result.codeResult && result.codeResult.code) {
-    //       Quagga.ImageDebug.drawPath(
-    //         result.line,
-    //         { x: 'x', y: 'y' },
-    //         drawingCtx,
-    //         { color: 'red', lineWidth: 3 },
-    //       );
-    //     }
-    //   }
-    // });
+        if (result.codeResult && result.codeResult.code) {
+          Quagga.ImageDebug.drawPath(
+            result.line,
+            { x: 'x', y: 'y' },
+            drawingCtx,
+            { color: 'red', lineWidth: 3 },
+          );
+        }
+      }
+    });
 
     Quagga.onDetected(onDetected);
   }, []);
